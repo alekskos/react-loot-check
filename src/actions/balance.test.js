98aa7bb94c12493e7e16ccd167acfeb6382b44b9
akostenko <aleksandr.kostenko@bia-tech.ru>
2019-02-04 15:32:50 +0300

    Added new actions

--- src/actions/balance.test.js
+++ src/actions/balance.test.js
@@ -4,7 +4,26 @@ import * as actions from './balance';
 it('creates an actions to set the balance', () => {
     const balance = 0;
 
-    const expectedAction = { type: constants.SET_BALANCE, balance };
+    const expectedAction = {
+        type: constants.SET_BALANCE,
+        balance
+    };
 
     expect(actions.setBalance(balance)).toEqual(expectedAction);
 })
+
+it('creates an action to deposit into balance', () => {
+    const deposit = 10;
+
+    const expectedAction = { type : constants.DEPOSIT, deposit };
+
+    expect(actions.deposit(deposit)).toEqual(expectedAction);
+});
+
+it('creates an action to withdraw from the balance', () => {
+    const withdrawal = 10;
+
+    const expectedAction = { type: constants.WITHDRAW, withdrawal };
+
+    expect(actions.withdraw(withdrawal)).toEqual(expectedAction)
+});
