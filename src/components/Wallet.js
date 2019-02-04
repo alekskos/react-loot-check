454000824e01b0774f992a58c2b8ce346d8119e4
akostenko <aleksandr.kostenko@bia-tech.ru>
2019-02-04 16:12:53 +0300

    Added Wallet component

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { deposit, withdraw } from '../actions/balance';
 
 export class Wallet extends Component {
+    state = {
+        balance: undefined
+    };
+
+    updateBalance = event => this.setState({ balance: +event.target.value });
+
+    deposit = () => this.props.deposit(this.state.balance);
+
+    withdraw = () => this.props.withdraw(this.state.balance);
+
     render() {
         return (
             <div>
                 <h2 className="balance">Wallet balance: {this.props.balance}</h2>
+                <br/>
+                <input 
+                    className='input-wallet'
+                    onChange={this.updateBalance}
+                />
+                <button 
+                    className='btn-deposit'
+                    onClick={this.deposit}
+                    children="Deposit"
+                />
+                <button 
+                    className="btn-withdraw"
+                    onClick={this.withdraw}
+                    children="Withdraw" 
+                />
             </div>
         )
     }
 }
 
-export default connect(state => { return { balance: state }}, null)(Wallet);
+export default connect(state => { return { balance: state }}, { deposit, withdraw })(Wallet);
--- src/components/Wallet.test.js
+++ src/components/Wallet.test.js
@@ -1,19 +1,53 @@
-import React from 'react'
-import Enzyme, { shallow } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-import { Wallet } from './Wallet'
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { Wallet } from './Wallet';
 
 Enzyme.configure({ adapter: new Adapter() })
 
 describe('Wallet', () => {
-  const props = { balance: 20 }
-  const wallet = shallow(<Wallet {...props} />)
+  const mockDeposit = jest.fn();
+  const mockWithdraw = jest.fn();
+  const props = { balance: 20, deposit: mockDeposit, withdraw: mockWithdraw };
+  const wallet = shallow(<Wallet {...props} />);
 
   it('render properly', () => {
     expect(wallet).toMatchSnapshot()
-  })
+  });
 
   it('dislays the balance from props', () => {
-    expect(wallet.find('.balance').text()).toEqual('Wallet balance: 20')
-  })
-})
+    expect(wallet.find('.balance').text()).toEqual('Wallet balance: 20');
+  });
+
+  it('creates an inpu to deposit into or withdraw from the balance', () => {
+    expect(wallet.find('.input-wallet').exists()).toBeTruthy();
+  });
+
+  describe('when the user types into the wallet input', () => {
+    const userBalance = '25';
+    
+    beforeEach(() => {
+      wallet.find('.input-wallet').simulate('change', { target: { value: userBalance }});
+    });
+
+    it('updates the local wallet balance in `state` and converts it to a number', () => {
+      expect(wallet.state().balance).toEqual(parseInt(userBalance, 10));
+    });
+
+    describe('and the user wants to make a deposit', () => {
+      beforeEach(() => wallet.find('.btn-deposit').simulate('click'));
+
+      it('dispatches the `deposit()` it receives from props with local balance', () => {
+        expect(mockDeposit).toHaveBeenCalledWith(+userBalance);
+      });
+    });
+
+    describe('and the user wants to make a withdrawal', () => {
+      beforeEach(() => wallet.find('.btn-withdraw').simulate('click'));
+  
+      it('dispatches the `withdraw()` it receives form props ti local balance', () => {
+        expect(mockWithdraw).toHaveBeenCalledWith(+userBalance);
+      });
+    });
+  });
+});
